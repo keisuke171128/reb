3d5398df6ec550c75a6da4d7fd589e80b1f14c3c
171129ino <171129kei@gmail.com>
2018-02-14 08:46:23 +0900

    made Contactページ

--- page-member.php
+++ page-member.php
@@ -12,7 +12,16 @@
  */
 
 get_header(); ?>
-
+<div id="primary" class="boxed-layout-header page-header header-small">
+				<div class="container">
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1 text-center">
+					<h1 class="hestia-title">Member</h1>
+				</div>
+			</div>
+		</div>
+		
+		<div data-parallax="active" class="header-filter header-filter-gradient"></div>	</div>
 <div class="whole-wrapper">
 	<div class="member-wrapper">
 		<?php $users = get_users( array('orderby'=>ID,'order'=>ASC) ); ?>
